b7de739c7ef7d719ff72ece997513df573361fa0
estebannahuel <estebanaugieri44@gmail.com>
2023-05-22 20:16:38 -0300

    confirmaar

--- ensaladera.js
+++ ensaladera.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 console.log("toma la ensaladera")
 console.log(`preparacion de seleccion`);
-=======
 console.log("toma la ensaladera y prepara todo")
 console.log("instrucciones")
->>>>>>> 59c77bf4df5340622df6b5696ea7f5fc0ca7dffa
